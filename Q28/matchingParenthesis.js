1c61d1fefda659cbe86a8ded9eaa956ea42b28ed
Isaac Yoon <ysaacioon@yahoo.com>
2017-08-06 16:06:41 -0700

    complete Matching Paren'

--- Q28/matchingParenthesis.js
+++ Q28/matchingParenthesis.js
@@ -1,5 +1,23 @@
 //interviewCake28
 
-function parenthesisMath(){
-
+function parenthesisMatch(str, ind){
+	let openParenAfterIndex = 0;
+	for(let i = 0; i < str.length; i++) {
+		if(str[i] === '(' && i > ind) {
+			openParenAfterIndex++;
+		} else if(str[i] === ')' && openParenAfterIndex ===0) {
+			return i;
+		} else if(str[i] === ')') {
+			openParenAfterIndex--;
+		}
+	}
+	throw error('no matching parenthesis');
 }
+
+let str = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.";
+let output = parenthesisMatch(str, 10);
+console.log(output);
+/*
+	(hello)
+	^
+*/
