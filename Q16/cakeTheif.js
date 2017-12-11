79694e92b15bf1c5d13a09fee10a0a92ac0a27cf
Isaac Yoon <ysaacioon@yahoo.com>
2017-12-10 22:54:10 -0800

    Q16

--- Q16/cakeTheif.js
+++ Q16/cakeTheif.js
@@ -1,38 +1,9 @@
-/*
-//This approach doesn't work, because we aren't counting the number of instance like the coins. We are checking the overall total amount.
 
-function cakeTheif(capacity, cakeTulples) {
-
-	let maximumWeight = 0;
-
-	for(var i=0; i < cakeTulples.length; i++) {
-		let potCap = capacity - cakeTuples[i][0];
-		let value = cakeTulples[i][1]
-		if(potCap >= 0) {
-			let currVal = cakeTheif(potCap, cakeTuples) + value;
-			console.log(currVal)
-			console.log('max', maximumWeight)
-			if(currVal > maximumWeight) {
-				maximumWeight = currVal;
-			}
-		} else {
-			return 0;
-		}
-	}
-	return maximumWeight;
-}
-
-let cakeTuples = [[7, 160], [3, 90], [2, 15]];
-let capacity = 20
-
-let output = cakeTheif(capacity, cakeTuples)
-console.log('output', output)
-*/
 
 function cakeTheif(capacity, cakeTulples) {
 
 	//no base case needed because forloop will automatically stop
-	let maximumWeight = 0;
+	let maximumVal = 0;
 
 	//create all possible combination
 	for(var i=0; i < cakeTulples.length; i++) {
@@ -42,13 +13,13 @@ function cakeTheif(capacity, cakeTulples) {
 		if(potCap >= 0) {
 			let currVal = cakeTheif(potCap, cakeTuples) + value;
 			//this if statement keeps track of the highest value returned
-			if(currVal > maximumWeight) {
-				maximumWeight = currVal;
+			if(currVal > maximumVal) {
+				maximumVal = currVal;
 			}
 		}
 	}
-	//return the maximumWeight
-	return maximumWeight;
+	//return the maximumVal
+	return maximumVal;
 }
 
 
@@ -63,7 +34,6 @@ function cakeTheifMemo(capacity, cakeTulples, memo) {
 			let value = cakeTulples[i][1]
 			if(potCap >= 0) {
 				let currVal = cakeTheifMemo(potCap, cakeTuples, memo) + value;
-
 				if(currVal > maximumVal) {
 					maximumVal = currVal;
 				}
